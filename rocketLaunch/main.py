834d250a1c76ca9479c72af40987a873569e526c
mmert9008 <michael.mertens.jr@gmail.com>
2025-04-19 23:04:11 -0500

    add project's initial provided main.py

--- rocketLaunch/main.py
+++ rocketLaunch/main.py
@@ -1,3 +1,33 @@
 #!/usr/bin/env python3
 
+# Import library code
+from p5 import *
+from random import randint
+
+# Set up global variables
+screen_size = 400
+
+# The draw_rocket function goes here
+
+
+
+# The draw_background function goes here
+
+
+
+def setup():
+    # Set up your animation here
+    size(screen_size, screen_size)
+    image_mode(CENTER)
+    global planet
+    planet = load_image('planet.png')
+   
+
+def draw():
+    # Things to do in every frame
+
+    
+
+
+run()
 
